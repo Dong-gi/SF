d341ba5fc3b5b986a19991a0b2bd1fd40528d31f
dev_donggi <hi.donggi@gmail.com>
2020-05-16 22:11:47 +0900

    Register 0.1.2

--- sf.js
+++ sf.js
@@ -23,24 +23,25 @@ class SF {
     static build(root) {
         if (!root)
             root = document.querySelector('[sf]');
+        root.__isRoot = true;
         let app = this.asSF(root);
         app.templates = SF.templates;
-        app.newTemplate = function (templateName, htmlMaker, callback) {
-            let template = ((htmlMaker, callback) => new Proxy(SF.__placeholder, {
+        app.newTemplate = function (templateName, htmlMaker) {
+            let template = ((htmlMaker) => new Proxy(SF.__placeholder, {
                 apply: function (target, thisArg, argumentsList) {
                     let node = htmlMaker().asSF();
-                    callback(node);
+                    if (argumentsList.length == 1)
+                        argumentsList[0](node);
+                    else if (argumentsList.length > 1)
+                        argumentsList[0](node, ...argumentsList.slice(1));
                     return node;
                 }
-            }))(htmlMaker, callback);
+            }))(htmlMaker);
             this.templates[templateName] = template;
             return template;
         }
-        app.asTemplate = function (templateName, id, callback) {
-            let target = document.getElementById(id);
-            if (!target)
-                throw new Error(`No such id element : ${id}`);
-            return this.newTemplate(templateName, () => target.outerHTML, callback);
+        app.asTemplate = function (templateName, element) {
+            return this.newTemplate(templateName, (e => () => e.outerHTML)(element));
         }
         return app;
     }
@@ -77,8 +78,11 @@ class SF {
                     return o[prop];
                 if (o[prop] != undefined && o[prop] != null)
                     return o[prop];
-                if (o.hasAttribute('sf') && document.getElementById(prop))
+                if (o.__isRoot && document.getElementById(prop))
                     return SF.asSF(document.getElementById(prop));
+                let children = Array.from(o.childNodes).filter(x => x.nodeName == prop.toUpperCase());
+                if (children.length > 0)
+                    return SF.asSFarr(children);
                 return SF.asSFarr(o.querySelectorAll(prop));
             }
         });
